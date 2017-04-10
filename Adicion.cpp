cf7a1a2045bd808db4cc58e10d037a54c72a7227
FrancoWalach <francowalach@yahoo.com>
2017-04-10 08:46:13 -0300

    Create Adicion.cpp

---
+++ Adicion.cpp
@@ -0,0 +1,21 @@
+
+/* TP1:"Adición"  
+* Franco Leonel Walach  
+* 20170408  
+*/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int Num1, Num2, Suma;
+	cout << "Ingrese un número entero. Luego presione enter" << endl;
+  cin >> Num1;
+  cout << "Ingrese otro número entero. Luego presione enter" <<endl;
+	cin >> Num2;
+	Suma = Num1 + Num2;
+	cout << "El resultado de la suma es" << endl << Suma;
+	return 0;
+}
